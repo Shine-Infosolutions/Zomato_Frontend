4f349fb73a730906ace6eef7279767787df75d24
Sayain-Aditya <as4316664@gmail.com>
2025-10-16 13:54:55 +0530

    feat: Update API base URL to localhost and refactor address management components

--- src/pages/OrderDetailsPage.jsx
+++ src/pages/OrderDetailsPage.jsx
@@ -131,7 +131,7 @@ const OrderDetailsPage = () => {
 
       try {
         const response = await fetch(
-          `https://24-7-b.vercel.app/api/user/order/${orderId}`
+          `http://localhost:4000/api/user/order/${orderId}`
         );
         const data = await response.json();
 
